e7c292cced2878578d8fed334593989e116d18f1
swpowe <spencer.powell@me.com>
2022-09-16 20:05:43 -0600

    final

--- controllers/showname.controller.js
+++ controllers/showname.controller.js
@@ -1,6 +1,6 @@
 const showName = (req, res) => {
     console.log("Home Route Launched");
-    res.send("<html><h1>Spencer Powell</h1></html>");
+    res.send("<html><h1>Spencer Powell says 'Hello World'</h1></html>");
   }
 
   module.exports = { showName };
--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const routes = require('./routes/index');
 
 const port = process.env.PORT || 3000;
 
-// app.get("/", (req, res) => {
-//     res.send('<html><h1>Spencer Powell</h1></html>')
-// })
 app.use(routes);
 
 app.listen(port, () => {
