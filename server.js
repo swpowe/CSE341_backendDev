6c69ecec6673460baa1fc8eb5f9529a38f37ffbf
swpowe <spencer.powell@me.com>
2022-09-19 17:49:51 -0600

    learning

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 const routes = require('./routes/index');
 
 const port = process.env.PORT || 3000;
+const mongodb_username = process.env.MONGO_USR;
+const mongodb_password = process.env.MONGO_PWD;
 
 
 app.use(routes);
@@ -17,8 +19,7 @@ app.listen(port, () => {
 });
 
 async function main() {
-    console.log('in main');
-    const uri = "mongodb+srv://swpowe:e38YheYRPcJNGIuC@cluster0.ku9wvjq.mongodb.net/?retryWrites=true&w=majority";
+    const uri = `mongodb+srv://${mongodb_username}:${mongodb_password}@cluster0.ku9wvjq.mongodb.net/?retryWrites=true&w=majority`;
 
     const client = new MongoClient(uri);
 
@@ -27,7 +28,16 @@ async function main() {
         await client.connect();
  
         // Make the appropriate DB calls
-        await  listDatabases(client);
+        //?? await  listDatabases(client);
+        await addContact(client,
+            {
+                name: "Spencers Lovely Loft",
+                summary: "A charming loft in Paris",
+                bedrooms: 1,
+                bathrooms: 1
+            }
+        );
+
  
     } catch (e) {
         console.error(e);
@@ -44,6 +54,11 @@ async function listDatabases(client){
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
+
+async function addContact(client, contact) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(contact);
+    console.log(`New contact created at: ${result.insertedId}`);
+}
 
 
 // Mongo Code
