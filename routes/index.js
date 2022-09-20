e8edac455f031bf27c38c3722f565b0f9991f910
swpowe <spencer.powell@me.com>
2022-09-19 18:32:04 -0600

    errors

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { showName } = require("../controllers/showName.controller")
+const { showName, listContacts } = require("../controllers/showName.controller")
 
 router.get("/", showName);
+
+router.get('/contacts', listContacts);
 
 module.exports = router;
