01b5fd50297016c10c3fa22ef0a8f3c3377d6037
swpowe <spencer.powell@me.com>
2022-09-19 18:31:57 -0600

    errors with mongodb

--- controllers/showName.controller.js
+++ controllers/showName.controller.js
@@ -1,6 +1,38 @@
-const showName = (req, res) => {
-    console.log("Home Route Launched");
-    res.send("<html><h1>Spencer Powell says 'Hello World'</h1></html>");
-  }
+const { MongoClient } = require("mongodb");
 
-  module.exports = { showName };
+const mongodb_username = process.env.MONGO_USR;
+const mongodb_password = process.env.MONGO_PWD;
+
+const showName = (req, res) => {
+  console.log("Home Route Launched");
+  res.send("<html><h1>Spencer Powell says 'Hello World'</h1></html>");
+};
+
+const listContacts = (req, res) => {
+  res.send("<html><h1>Contacts Page</h1></html>");
+
+  async function mongoDb_Connect() {
+    const uri = `mongodb+srv://${mongodb_username}:${mongodb_password}@cluster0.ku9wvjq.mongodb.net/?retryWrites=true&w=majority`;
+
+    const client = new MongoClient(uri);
+
+    try {
+      await client.connect();
+      await listDatabases(client);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await client.close();
+    }
+  }
+  mongoDb_Connect().catch(console.error);
+
+  async function listDatabases(client) {
+    databaseList = await client.db.admin().listDatabases();
+
+    console.log("Databases:");
+    databaseList.databases.forEach((db) => console.log(` - ${db.name}`));
+  }
+};
+
+module.exports = { showName, listContacts };
