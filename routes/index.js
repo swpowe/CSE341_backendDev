75b8f8c991f6f5bf5841c10605dbd9bb68fbd607
swpowe <spencer.powell@me.com>
2022-09-24 15:16:39 -0600

    in progress

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,45 @@
-const express = require("express");
+const express = require('express');
+// eslint-disable-next-line new-cap
 const router = express.Router();
 
-const {showName, showContacts, showContact} = require("../controllers/showName.controller")
+// const app = express();
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-router.get("/", showName);
+
+const {
+  showName,
+  showContacts,
+  showContact,
+} = require('../controllers/showName.controller');
+
+const {
+  addContact,
+  modifyContact,
+  deleteContact,
+} = require('../controllers/contact.controller');
+
+router.get('/', showName);
 
 router.get('/contacts', showContacts);
 router.get('/contact', showContact);
+
+// Lesson 3 Personal Assignment
+// ## POST route to create a new contact
+// ## returns the new contact ID
+// ## returns success status code
+// router.get('/contact-add', addContact);
+router.post('/contact-add', addContact);
+
+// ## PUT route to update a contact
+// ## ex. api-url-path/contacts/id-to-modify
+// ## returns success status code
+router.put('/contact-modify', modifyContact);
+
+// ## DELETE route to delete a contact
+// ## returns a success status code
+router.delete('/contact-delete', deleteContact);
+
+// ## rest file
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const routes = require('./routes/index');
 
@@ -10,5 +14,5 @@ const port = process.env.PORT || 3000;
 app.use(routes);
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 });
