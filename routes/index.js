6f88a8e79120ff61c6687afa39e4747b75b0d6f2
swpowe <spencer.powell@me.com>
2022-10-04 08:17:11 -0600

    working

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,21 @@
-const express = require("express");
-// eslint-disable-next-line new-cap
-const router = express.Router();
+const routes = require('express').Router();
+const temple = require('./temple');
 
-const {
-  showName,
-  // displayContacts,
-  displaySingleContact,
-} = require("../controllers/showName.controller");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
 
-const {
-  displayContacts,
-  addContact,
-  modifyContact,
-  deleteContact,
-} = require("../controllers/contact.controller");
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-router.get("/", showName);
+routes.use('/temples', temple);
+routes.use(
+  '/',
+  (docData = (req, res) => {
+    let docData = {
+      documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
+    };
+    res.send(docData);
+  })
+);
 
-router.get("/contacts", displayContacts);
-router.get("/contact", displaySingleContact);
-
-// Lesson 3 Personal Assignment
-// ## POST route to create a new contact
-// ## returns the new contact ID
-// ## returns success (201) status code
-router.post("/addContact", addContact);
-
-// ## PUT route to update a contact
-// ## ex. api-url-path/contacts/id-to-modify
-// ## returns success (204) status code
-router.put("/modifyContact", modifyContact);
-
-// ## DELETE route to delete a contact
-// ## returns a success (200) status code
-// router.delete("/:id", (req, res) => {
-//   console.log(`ID: ${req.params.id}`);
-// });
-
-router.delete('/deleteContact', deleteContact);
-
-// ## rest file
-
-module.exports = router;
+module.exports = routes;
