f13d0a02230dcd0f8d1955d8774014ec9b751ab6
swpowe <spencer.powell@me.com>
2022-10-14 21:01:59 -0600

    finished week 5 local

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,19 +1,21 @@
-const mongodb = require('../db/connect');
+const mongodb = require("../db/connect");
+const {ObjectId} = require('mongodb');
 
 const getAllTodos = async (req, res) => {
-  const todos = await mongodb.getDb().db().collection('todos').find();
+  const todos = await mongodb.getDb().db().collection("todos").find();
   todos.toArray().then((items) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(items);
   });
 };
 
 const getOneTodo = async (req, res) => {
-  res.send('get One todo');
+  const todo = await mongodb.getDb().db().collection("todos").findOne({ _id: new ObjectId(req.params.id)});
+  res.status(201).json(todo);
 };
 
 const addTodo = async (req, res) => {
-  const todos = await mongodb.getDb().db().collection('todos');
+  const todos = await mongodb.getDb().db().collection("todos");
   const newTodo = req.body;
   todos.insertOne(newTodo, (err) => {
     if (err) {
