760ac70fce164915a799b6e93d1d10e354cf596c
swpowe <spencer.powell@me.com>
2022-10-18 21:52:28 -0600

    mongooose up and running

--- models/index.js
+++ models/index.js
@@ -1,13 +1,29 @@
-const dbConfig = require('../config/db.config');
-
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
 
-const db = {};
-db.mongoose = mongoose;
-db.url = dbConfig.url;
+const addToDoItem = async () => {
+  await mongoose.connect(process.env.MONGODB_URI, {dbName: 'new_mongoose'});
 
-// db.theme = require('./theme.js')(mongoose);
-// db.user = require('./user.js')(mongoose);
+  const ToDoItem = new Schema({
+    title: String,
+    description: {
+      type: String,
+      required: true,
+    },
+  });
 
-module.exports = db;
+  const MyModel = mongoose.model('ToDo', ToDoItem);
+
+  const item = new MyModel();
+  item.title = ' pullled apart';
+  item.description = 'ToDo item description';
+
+  try {
+    const id = await item.save();
+    console.log(`ID created: ${id}`);
+  } catch (error) {
+    console.log(error._message);
+  }
+};
+
+module.exports = addToDoItem;
