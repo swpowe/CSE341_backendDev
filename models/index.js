50d49bf49db8ee2bc1266348f45f230f2046ba28
swpowe <spencer.powell@me.com>
2022-10-18 19:47:08 -0600

    in progress with mongoose

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const dbConfig = require('../config/db.config');
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
+
+// db.theme = require('./theme.js')(mongoose);
+// db.user = require('./user.js')(mongoose);
+
+module.exports = db;
