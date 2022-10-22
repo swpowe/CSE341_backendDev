73772cf0a9d836e7f69354fef8d9fc071c021151
swpowe <spencer.powell@me.com>
2022-10-21 18:49:21 -0600

    fixed external model schema

--- models/index.js
+++ models/index.js
@@ -1,30 +1,32 @@
-const mongoose = require('mongoose');
-const ToDoItem = require('./toDoItem')(mongoose);
+const mongoose = require("mongoose");
+// const ToDoItem = require('./toDoItem')(mongoose);
+const ToDoItem = require("./toDoItem");
 // const Schema = mongoose.Schema;
 
 const addToDoItem = async () => {
-  await mongoose.connect(process.env.MONGODB_URI, {dbName: 'new_mongoose'});
+  await mongoose.connect(process.env.MONGODB_URI, { dbName: "new_mongoose" });
 
   // !! Pull from external file
-  const ToDoItem = new Schema({
-    title: String,
-    description: {
-      type: String,
-      required: true,
-    },
-  });
+  //   const ToDoItem = new Schema({
+  //     title: String,
+  //     description: {
+  //       type: String,
+  //       required: true,
+  //     },
+  //   });
 
-  const MyModel = mongoose.model('ToDo', ToDoItem);
+  //   const MyModel = mongoose.model('ToDo', ToDoItem);
 
-  const item = new MyModel();
-  item.title = ' pullled apart';
-  item.description = 'ToDo item description';
+  //   const item = new MyModel();
+  const item = new ToDoItem();
+  item.title = "pullled apart";
+  item.description = "ToDo item description";
 
   try {
     const id = await item.save();
     console.log(`ID created: ${id}`);
   } catch (error) {
-    console.log(error._message);
+    console.log(error.errors.description.message);
   }
 };
 
--- models/toDoItem.js
+++ models/toDoItem.js
@@ -1,32 +1,69 @@
-module.exports = (mongoose) => {
-  const ToDoItem = mongoose.model('todos', mongoose.Schema({
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-    },
-    priority: {
-      type: String,
-      enum: ['Low', 'Medium', 'High'],
-      default: 'Low',
-    },
-    startDate: {
-      type: String,
-    },
-    endDate: {
-      type: String,
-    },
-    completed: {
-      type: String,
-    },
-    assignedTo: {
-      type: String,
-    },
-    LIST_ID: {
-      type: String,
-    },
-  }));
-  return ToDoItem;
-};
+const mongoose = require('mongoose');
+
+const toDoItemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: [true, 'You need to add a description.'],
+  },
+  priority: {
+    type: String,
+    enum: ['Low', 'Medium', 'High'],
+    default: 'Low',
+  },
+  startDate: {
+    type: String,
+  },
+  endDate: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  assignedTo: {
+    type: String,
+  },
+  LIST_ID: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('ToDoItem', toDoItemSchema);
+
+
+// module.exports = (mongoose) => {
+//   const ToDoItem = mongoose.model('todos', mongoose.Schema({
+//     title: {
+//       type: String,
+//       required: true,
+//     },
+//     description: {
+//       type: String,
+//     },
+//     priority: {
+//       type: String,
+//       enum: ['Low', 'Medium', 'High'],
+//       default: 'Low',
+//     },
+//     startDate: {
+//       type: String,
+//     },
+//     endDate: {
+//       type: String,
+//     },
+//     completed: {
+//       type: String,
+//     },
+//     assignedTo: {
+//       type: String,
+//     },
+//     LIST_ID: {
+//       type: String,
+//     },
+//   }));
+//   return ToDoItem;
+// };
